78219296784e70728ec801462b0fa008437528d9
prakhar <prakhar801@gmail.com>
2019-02-24 15:09:59 +0530

    uba files with two web pages

---
+++ main.js
@@ -0,0 +1,14 @@
+
+<script src="https://www.gstatic.com/firebasejs/5.8.4/firebase.js"></script>
+<script>
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyB7S9oe5QuuD1E4P3yyEZcy68gWSo4nWgw",
+    authDomain: "uba-iitk.firebaseapp.com",
+    databaseURL: "https://uba-iitk.firebaseio.com",
+    projectId: "uba-iitk",
+    storageBucket: "uba-iitk.appspot.com",
+    messagingSenderId: "736581974039"
+  };
+  firebase.initializeApp(config);
+</script>
